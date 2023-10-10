32d4d901f69a0585bfb7e26568a5e50993088dcb
Sérgio Augusto David Jr <sergio.adjr@gmail.com>
2023-10-10 13:30:11 -0400

    finalizando

--- js/index.js
+++ js/index.js
@@ -1,22 +1,29 @@
-const wrapper =document.querySelector(".wrapper"),
-qrInput = wrapper.querySelector(".form input"),
-generateBtn =wrapper.querySelector(".form button"),
-qrImg = wrapper.querySelector(".qr-code img");
+const wrapper = document.querySelector(".wrapper"),
+      telefoneInput = wrapper.querySelector("#telefone"),
+      generateBtn = wrapper.querySelector(".form button"),
+      qrImg = wrapper.querySelector(".qr-code img");
+
+telefoneInput.addEventListener("input", () => {
+    let value = telefoneInput.value.replace(/\D/g, '');
+    value = value.replace(/^(\d{2})(\d)/g, '($1) $2');
+    value = value.replace(/(\d)(\d{4})$/, '$1-$2');
+    telefoneInput.value = value;
+});
 
 generateBtn.addEventListener("click", () => {
-    let qrValue = qrInput.value;
-    if(!qrValue) return;
+    let phoneNumber = telefoneInput.value.replace(/\D/g, '');
+    if (!phoneNumber) return;
     generateBtn.innerText = "Gerando um Qr Code..."
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    let whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}`;
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${encodeURIComponent(whatsappLink)}`;
     qrImg.addEventListener("load", () => {
         generateBtn.innerText = "Gerar Qr Code"
         wrapper.classList.add("active");
     });
-   
 });
 
-qrInput.addEventListener("keyup", () => {
-    if(!qrInput.value){
+telefoneInput.addEventListener("keyup", () => {
+    if (!telefoneInput.value) {
         wrapper.classList.remove("active");
     }
 });
